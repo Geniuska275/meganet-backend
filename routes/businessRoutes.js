25927ed845f7ecc46c1f5bf71f263b97f35d68f0
Geniuska275 <aigbojie2020@gmail.com>
2026-09-15 11:59:24 +0100

    worked on recently

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -60,31 +60,31 @@ router.post(
         l_origin,
         cost,
 
-        file: req.file
+        file: req.files.file[0]
           ? {
-              originalName: req.file.originalname,
-              fileName: req.file.filename,
-              path: req.file.path,
-              mimeType: req.file.mimetype,
-              size: req.file.size,
+              originalName: req.files.file[0].originalname,
+              fileName: req.files.file[0].filename,
+              path: req.files.file[0].path,
+              mimeType: req.files.file[0].mimetype,
+              size: req.files.file[0].size,
             }
           : null,
-          file2: req.file
+          file2: req.files.file2[0]
           ? {
-              originalName: req.file.originalname,
-              fileName: req.file.filename,
-              path: req.file.path,
-              mimeType: req.file.mimetype,
-              size: req.file.size,
+              originalName: req.files.file2[0].originalname,
+              fileName: req.files.file2[0].filename,
+              path: req.files.file2[0].path,
+              mimeType: req.files.file2[0].mimetype,
+              size: req.files.file2[0].size,
             }
           : null,
-          file3: req.file
+          file3: req.files.file3[0]
           ? {
-              originalName: req.file.originalname,
-              fileName: req.file.filename,
-              path: req.file.path,
-              mimeType: req.file.mimetype,
-              size: req.file.size,
+              originalName: req.files.file3[0].originalname,
+              fileName: req.files.file3[0].filename,
+              path: req.files.file3[0].path,
+              mimeType: req.files.file3[0].mimetype,
+              size: req.files.file3[0].size,
             }
           : null,
       });
